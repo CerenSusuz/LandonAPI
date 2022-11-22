8936c946a7bd3ab1f291888b679c43694874cc02
CerenSusuz <ceren_susuz@epam.com>
2022-11-22 15:30:54 +0300

    exception filter

--- LandonWebAPI/Program.cs
+++ LandonWebAPI/Program.cs
@@ -1,3 +1,5 @@
+using LandonWebAPI.Filters;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddMvc(options =>
+    {
+        options.Filters.Add<JsonExceptionFilter>();
+    });
 
 builder.Services.AddRouting(options =>
     options.LowercaseUrls = true);
